85eedcb7ce0eb5612cf5fa06a9aa9ac9d85557d9
Johnathan <johnathan.stark42@gmail.com>
2023-11-08 16:48:29 -0300

    m

--- src/repository/post.rs
+++ src/repository/post.rs
@@ -1,4 +1,3 @@
-
 use diesel::{ExpressionMethods, QueryDsl};
 
 use tokio_diesel::{AsyncResult, AsyncRunQueryDsl};
@@ -17,11 +16,19 @@ macro_rules! get_all_category_asc_desc {
             match category_parsed.is_ok() {
                 true => {
                     let category_id = category_parsed.unwrap();
-                    let g = $table.inner_join($categoria_table).filter(categoria_id.eq(category_id)).order($id);
+                    let g = $table.inner_join($categoria_table)
+                    .or_filter(tipo.eq(TipoPost::Texto)).or_filter(tipo.eq(TipoPost::Html))
+                    .filter(categoria_id.eq(category_id)).order($id);
+
+                       println!("ID");
+
                     GetCategory::ID(g)
                 }
                 false => {
                     let g = $table.inner_join($categoria_table).order($id);
+
+                         println!("ALL");
+
                     GetCategory::ALL(g)
                 }
             }
@@ -46,78 +53,73 @@ pub async fn get_all_posts(
 }
 
 pub async fn get_all_posts_lang(
-    conn: &PgAsyncConnection,lang:Language
+    conn: &PgAsyncConnection, lang: Language,
 ) -> AsyncResult<Vec<Post>> {
     post::table.filter(post::dsl::language.eq(lang)).load_async(conn).await
 }
 
 
-
 pub async fn get_last_n_posts(
     conn: &PgAsyncConnection,
     n: i64,
-    offset:i64,
-    ord:String,
-    category:String,
-    lang:Language
-) -> Result<Vec<PostWithCategory>,String> {
+    offset: i64,
+    ord: String,
+    category: String,
+    lang: Language,
+) -> Result<Vec<PostWithCategory>, String> {
     use crate::schema::post::dsl::*;
 
- let  res =  match ord.as_str() {
+    let res = match ord.as_str() {
         "asc" => {
-            let querry_category : GetCategory<_,_> = get_all_category_asc_desc!(category,post,id.asc(),crate::schema::category::table);
-              match querry_category {
-                GetCategory::ALL(x) => x.select((
-                    crate::schema::post::all_columns,
-                    crate::schema::category::all_columns
-                )).filter(language.eq(lang)).or_filter(tipo.eq(TipoPost::Texto)).or_filter(tipo.eq(TipoPost::Html))
-                    .limit(n).offset(offset).load_async::<(Post,Category)>(conn).await,
-                GetCategory::ID(x) => x.select((
-                                                    crate::schema::post::all_columns,
-                                                    crate::schema::category::all_columns
-                )).or_filter(tipo.eq(TipoPost::Texto)).or_filter(tipo.eq(TipoPost::Html))
-                    .filter(language.eq(lang)).limit(n).offset(offset).load_async::<(Post,Category)>(conn).await
-            }
-        }
-        "desc" =>{
-            let querry_category : GetCategory<_,_> = get_all_category_asc_desc!(category,post,id.desc(),crate::schema::category::table);
+            let querry_category: GetCategory<_, _> = get_all_category_asc_desc!(category,post,id.asc(),crate::schema::category::table);
             match querry_category {
                 GetCategory::ALL(x) => x.select((
                     crate::schema::post::all_columns,
                     crate::schema::category::all_columns
-                )).or_filter(tipo.eq(TipoPost::Texto)).or_filter(tipo.eq(TipoPost::Html))
-                    .filter(language.eq(lang)).limit(n).offset(offset).load_async::<(Post,Category)>(conn).await,
+                )).filter(language.eq(lang))
+                    .limit(n).offset(offset).load_async::<(Post, Category)>(conn).await,
                 GetCategory::ID(x) => x.select((
                     crate::schema::post::all_columns,
                     crate::schema::category::all_columns
-                )).or_filter(tipo.eq(TipoPost::Texto)).or_filter(tipo.eq(TipoPost::Html))
-                    .filter(language.eq(lang)).limit(n).offset(offset).load_async::<(Post,Category)>(conn).await
+                )).filter(language.eq(lang)).limit(n).offset(offset).load_async::<(Post, Category)>(conn).await
+            }
+        }
+        "desc" => {
+            let querry_category: GetCategory<_, _> = get_all_category_asc_desc!(category,post,id.desc(),crate::schema::category::table);
+            match querry_category {
+                GetCategory::ALL(x) => x.select((
+                    crate::schema::post::all_columns,
+                    crate::schema::category::all_columns
+                )).filter(language.eq(lang)).limit(n).offset(offset).load_async::<(Post, Category)>(conn).await,
+                GetCategory::ID(x) => x.select((
+                    crate::schema::post::all_columns,
+                    crate::schema::category::all_columns
+                )).filter(language.eq(lang)).limit(n).offset(offset)
+                    .load_async::<(Post, Category)>(conn).await
             }
         }
         _ => {
-            let querry_category : GetCategory<_,_> = get_all_category_asc_desc!(category,post,id.asc(),crate::schema::category::table);
+            let querry_category: GetCategory<_, _> = get_all_category_asc_desc!(category,post,id.asc(),crate::schema::category::table);
             match querry_category {
                 GetCategory::ALL(x) => x.select((
                     crate::schema::post::all_columns,
                     crate::schema::category::all_columns
-                )).or_filter(tipo.eq(TipoPost::Texto)).or_filter(tipo.eq(TipoPost::Html)).filter(language.eq(lang)).limit(n).offset(offset).load_async::<(Post,Category)>(conn).await,
+                )).filter(language.eq(lang)).limit(n).offset(offset).load_async::<(Post, Category)>(conn).await,
                 GetCategory::ID(x) => x.select((
                     crate::schema::post::all_columns,
                     crate::schema::category::all_columns
-                )).or_filter(tipo.eq(TipoPost::Texto)).or_filter(tipo.eq(TipoPost::Html)).filter(language.eq(lang)).limit(n).offset(offset).load_async::<(Post,Category)>(conn).await
+                )).filter(language.eq(lang)).limit(n).offset(offset).load_async::<(Post, Category)>(conn).await
             }
         }
     };
 
 
+    let r = res.unwrap();
+    let response = r.iter().map(|x1| {
+        let pos = &x1.0;
+        let category = &x1.1;
 
-   let r =  res.unwrap();
-     let response =   r.iter().map( |  x1| {
-
-       let pos = &x1.0;
-       let category = &x1.1;
-
-        PostWithCategory{
+        PostWithCategory {
             description: pos.description.clone(),
             id: pos.id,
             titulo: pos.titulo.clone(),
@@ -130,13 +132,10 @@ pub async fn get_last_n_posts(
             conteudo: pos.conteudo.clone(),
             name_category: category.name_url.clone(),
         }
-
     });
 
 
-
-  Ok(  response.collect() )
-
+    Ok(response.collect())
 }
 
 
@@ -165,14 +164,12 @@ pub async fn get_post_by_category(
 
 pub async fn get_post_by_viwes(
     conn: &PgAsyncConnection,
-    category:String,
+    category: String,
     limit: i64,
-    lang:Language
+    lang: Language,
 ) -> AsyncResult<Vec<Post>> {
-
-     match category.parse::<i32>().is_ok() {
+    match category.parse::<i32>().is_ok() {
         true => {
-
             post::table
                 .filter(post::categoria_id.eq(category.parse::<i32>().unwrap()))
                 .or_filter(post::tipo.eq(TipoPost::Texto)).or_filter(post::tipo.eq(TipoPost::Html))
@@ -182,35 +179,25 @@ pub async fn get_post_by_viwes(
                 .load_async(conn).await
         }
         false => {
-
-
             post::table
                 .order(post::total_views.desc())
                 .or_filter(post::tipo.eq(TipoPost::Texto)).or_filter(post::tipo.eq(TipoPost::Html))
                 .filter(post::dsl::language.eq(lang))
                 .limit(limit)
                 .load_async(conn).await
-
         }
-
     }
-
-
-
 }
-
 
 
 pub async fn get_post_by_audio(
     conn: &PgAsyncConnection,
-    category:String,
+    category: String,
     limit: i64,
-    lang:Language
+    lang: Language,
 ) -> AsyncResult<Vec<Post>> {
-
     match category.parse::<i32>().is_ok() {
         true => {
-
             post::table
                 .filter(post::categoria_id.eq(category.parse::<i32>().unwrap()))
                 .or_filter(post::tipo.eq(TipoPost::Audio))
@@ -220,23 +207,15 @@ pub async fn get_post_by_audio(
                 .load_async(conn).await
         }
         false => {
-
-
             post::table
                 .order(post::data_criacao.desc())
                 .or_filter(post::tipo.eq(TipoPost::Audio))
                 .filter(post::dsl::language.eq(lang))
                 .limit(limit)
                 .load_async(conn).await
-
         }
-
     }
-
-
-
 }
-
 
 
 pub async fn update_post_by_id(
@@ -254,8 +233,7 @@ pub async fn increment_total_views(
     conn: &PgAsyncConnection,
     post_id: i32,
 ) -> AsyncResult<Post> {
-
-   match diesel::update(post::table.filter(post::id.eq(post_id)))
+    match diesel::update(post::table.filter(post::id.eq(post_id)))
         .set(post::total_views.eq(post::total_views + 1))
         .execute_async(conn)
         .await {
